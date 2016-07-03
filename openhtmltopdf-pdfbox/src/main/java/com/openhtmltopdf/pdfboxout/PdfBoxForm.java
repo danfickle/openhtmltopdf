028739fcc5f328549898e602381e039abf110b43
Dan Fickle <newtab@gmail.com>
2016-07-03 13:37:26 +1000

    For #24 - Got submit buttons working. [ci skip]
    Also:
    - Refactored.
    - Added support for title attribute on text fields.

--- openhtmltopdf-pdfbox/src/main/java/com/openhtmltopdf/pdfboxout/PdfBoxForm.java
+++ openhtmltopdf-pdfbox/src/main/java/com/openhtmltopdf/pdfboxout/PdfBoxForm.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Level;
 
 import org.apache.pdfbox.cos.COSString;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -24,6 +25,7 @@ import com.openhtmltopdf.css.parser.FSColor;
 import com.openhtmltopdf.css.parser.FSRGBColor;
 import com.openhtmltopdf.render.Box;
 import com.openhtmltopdf.render.RenderingContext;
+import com.openhtmltopdf.util.XRLog;
 
 
 public class PdfBoxForm {
@@ -78,6 +80,128 @@ public class PdfBoxForm {
         }
     }
     
+    private static String getColorOperator(FSColor color) {
+        String colorOperator = "";
+        
+        if (color instanceof FSRGBColor) {
+            FSRGBColor rgb = (FSRGBColor) color;
+            float r = (float) rgb.getRed() / 255;
+            float g = (float) rgb.getGreen() / 255;
+            float b = (float) rgb.getBlue() / 255;
+            
+            colorOperator =
+                    String.format(Locale.US, "%.4f", r) + ' ' + 
+                    String.format(Locale.US, "%.4f", g) + ' ' + 
+                    String.format(Locale.US, "%.4f", b) + ' ' +
+                    "rg";
+        } else if (color instanceof FSCMYKColor) {
+            FSCMYKColor cmyk = (FSCMYKColor) color;
+            float c = cmyk.getCyan();
+            float m = cmyk.getMagenta();
+            float y = cmyk.getYellow();
+            float k = cmyk.getBlack();
+            
+            colorOperator = 
+                    String.format(Locale.US, "%.4f", c) + ' ' +
+                    String.format(Locale.US, "%.4f", m) + ' ' +
+                    String.format(Locale.US, "%.4f", y) + ' ' +
+                    String.format(Locale.US, "%.4f", k) + ' ' +
+                    "k";
+        }
+        
+        return colorOperator;
+    }
+    
+    private void processTextControl(ControlFontPair pair, Control ctrl, PDAcroForm acro, int i, Box root, PdfBoxOutputDevice od) throws IOException {
+        PDTextField field = new PDTextField(acro);
+        
+        FSColor color = ctrl.box.getStyle().getColor();
+        String colorOperator = getColorOperator(color);
+
+        String fontInstruction = "/" + pair.fontName + " 0 Tf";
+        field.setDefaultAppearance(fontInstruction + ' ' + colorOperator);
+        
+        field.setPartialName("OpenHTMLCtrl" + i); // Internal name.
+        controlNames.add("OpenHTMLCtrl" + i);
+        
+        field.setDefaultValue(ctrl.box.getElement().getAttribute("value")); // The reset value.
+        field.setValue(ctrl.box.getElement().getAttribute("value"));        // The original value.
+    
+        if (getNumber(ctrl.box.getElement().getAttribute("max-length")) != null) {
+            field.setMaxLen(getNumber(ctrl.box.getElement().getAttribute("max-length")));
+        }
+        
+        if (ctrl.box.getElement().hasAttribute("required")) {
+            field.setRequired(true);
+        }
+        
+        if (ctrl.box.getElement().hasAttribute("readonly")) {
+            field.setReadOnly(true);
+        }
+        
+        if (ctrl.box.getElement().getNodeName().equals("textarea")) {
+            field.setMultiline(true);
+        } else if (ctrl.box.getElement().getAttribute("type").equals("password")) {
+            field.setPassword(true);
+        }
+        
+        field.setMappingName(ctrl.box.getElement().getAttribute("name")); // Export name.
+        
+        if (ctrl.box.getElement().hasAttribute("title")) {
+            field.setAlternateFieldName(ctrl.box.getElement().getAttribute("title"));
+        }
+        
+        PDAnnotationWidget widget = field.getWidgets().get(0);
+
+        Rectangle2D rect2D = PdfBoxLinkManager.createTargetArea(ctrl.c, ctrl.box, ctrl.pageHeight, ctrl.transform, root, od);
+        PDRectangle rect = new PDRectangle((float) rect2D.getMinX(), (float) rect2D.getMinY(), (float) rect2D.getWidth(), (float) rect2D.getHeight());
+
+        widget.setRectangle(rect);
+        widget.setPage(ctrl.page);
+      
+        ctrl.page.getAnnotations().add(widget);
+        acro.getFields().add(field);
+    }
+    
+    private void processSubmitControl(PDAcroForm acro, int i, Control ctrl, Box root, PdfBoxOutputDevice od) throws IOException {
+        final int FLAG_USE_GET = 1 << 3;
+        final int FLAG_USE_HTML_SUBMIT = 1 << 2;
+        
+        PDPushButton btn = new PDPushButton(acro);
+        btn.setPushButton(true);
+        btn.setPartialName("OpenHTMLCtrl" + i);
+        
+        PDAnnotationWidget widget = btn.getWidgets().get(0);
+        
+        Rectangle2D rect2D = PdfBoxLinkManager.createTargetArea(ctrl.c, ctrl.box, ctrl.pageHeight, ctrl.transform, root, od);
+        PDRectangle rect = new PDRectangle((float) rect2D.getMinX(), (float) rect2D.getMinY(), (float) rect2D.getWidth(), (float) rect2D.getHeight());
+
+        widget.setRectangle(rect);
+        widget.setPage(ctrl.page);
+        
+        PDFileSpecification fs = PDFileSpecification.createFS(new COSString(element.getAttribute("action")));
+
+        COSArrayList<String> fieldsToInclude = new COSArrayList<String>();
+        fieldsToInclude.addAll(controlNames);
+        
+        PDActionSubmitForm submit = new PDActionSubmitForm();
+        submit.setFields(fieldsToInclude.toList());
+        submit.setFile(fs);
+
+        if (!element.getAttribute("method").equalsIgnoreCase("post")) {
+            // Default method is get.
+            XRLog.general(Level.WARNING, "Using GET request method for form. You probably meant to add a method=\"post\" attribute to your form");
+            submit.setFlags(FLAG_USE_GET | FLAG_USE_HTML_SUBMIT);
+        } else {
+            submit.setFlags(FLAG_USE_HTML_SUBMIT);
+        }
+
+        widget.setAction(submit);
+
+        ctrl.page.getAnnotations().add(widget);
+        acro.getFields().add(btn);
+    }
+    
     public int process(PDAcroForm acro, int startId, Box root, PdfBoxOutputDevice od) throws IOException {
         int  i = startId;
 
@@ -85,84 +209,16 @@ public class PdfBoxForm {
             i++;
             
             Control ctrl = pair.control;
+            Element e = ctrl.box.getElement();
             
-            if ((ctrl.box.getElement().getNodeName().equals("input") &&
-                 ctrl.box.getElement().getAttribute("type").equals("text")) ||
-                (ctrl.box.getElement().getNodeName().equals("textarea")) ||
-                (ctrl.box.getElement().getNodeName().equals("input") && 
-                 ctrl.box.getElement().getAttribute("type").equals("password"))) {
+            if ((e.getNodeName().equals("input") &&
+                 e.getAttribute("type").equals("text")) ||
+                (e.getNodeName().equals("textarea")) ||
+                (e.getNodeName().equals("input") && 
+                 e.getAttribute("type").equals("password"))) {
 
                 // Start with the text controls (text, password and textarea).
-                PDTextField field = new PDTextField(acro);
-                
-                FSColor color = ctrl.box.getStyle().getColor();
-                String colorOperator = "";
-                
-                if (color instanceof FSRGBColor) {
-                    FSRGBColor rgb = (FSRGBColor) color;
-                    float r = (float) rgb.getRed() / 255;
-                    float g = (float) rgb.getGreen() / 255;
-                    float b = (float) rgb.getBlue() / 255;
-                    
-                    colorOperator =
-                            String.format(Locale.US, "%.4f", r) + ' ' + 
-                            String.format(Locale.US, "%.4f", g) + ' ' + 
-                            String.format(Locale.US, "%.4f", b) + ' ' +
-                            "rg";
-                } else if (color instanceof FSCMYKColor) {
-                    FSCMYKColor cmyk = (FSCMYKColor) color;
-                    float c = cmyk.getCyan();
-                    float m = cmyk.getMagenta();
-                    float y = cmyk.getYellow();
-                    float k = cmyk.getBlack();
-                    
-                    colorOperator = 
-                            String.format(Locale.US, "%.4f", c) + ' ' +
-                            String.format(Locale.US, "%.4f", m) + ' ' +
-                            String.format(Locale.US, "%.4f", y) + ' ' +
-                            String.format(Locale.US, "%.4f", k) + ' ' +
-                            "k";
-                }
-
-                String fontInstruction = "/" + pair.fontName + " 12 Tf";
-                field.setDefaultAppearance(fontInstruction + ' ' + colorOperator);
-                
-                field.setPartialName("OpenHTMLCtrl" + i); // Internal name.
-                controlNames.add("OpenHTMLCtrl" + i);
-                
-                field.setDefaultValue(ctrl.box.getElement().getAttribute("value")); // The reset value.
-                field.setValue(ctrl.box.getElement().getAttribute("value"));        // The original value.
-            
-                if (getNumber(ctrl.box.getElement().getAttribute("max-length")) != null) {
-                    field.setMaxLen(getNumber(ctrl.box.getElement().getAttribute("max-length")));
-                }
-                
-                if (ctrl.box.getElement().hasAttribute("required")) {
-                    field.setRequired(true);
-                }
-                
-                if (ctrl.box.getElement().hasAttribute("readonly")) {
-                    field.setReadOnly(true);
-                }
-                
-                if (ctrl.box.getElement().getNodeName().equals("textarea")) {
-                    field.setMultiline(true);
-                } else if (ctrl.box.getElement().getAttribute("type").equals("password")) {
-                    field.setPassword(true);
-                }
-                
-                field.setMappingName(ctrl.box.getElement().getAttribute("name")); // Export name.
-                
-                PDAnnotationWidget widget = field.getWidgets().get(0);
-
-                Rectangle2D rect2D = PdfBoxLinkManager.createTargetArea(ctrl.c, ctrl.box, ctrl.pageHeight, ctrl.transform, root, od);
-                PDRectangle rect = new PDRectangle((float) rect2D.getMinX(), (float) rect2D.getMinY(), (float) rect2D.getWidth(), (float) rect2D.getHeight());
-
-                widget.setRectangle(rect);
-                widget.setPage(ctrl.page);
-              
-                ctrl.page.getAnnotations().add(widget);
-                acro.getFields().add(field);
+                processTextControl(pair, ctrl, acro, i, root, od);
             }
             else if ((ctrl.box.getElement().getNodeName().equals("input") && 
                       ctrl.box.getElement().getAttribute("type").equals("submit")) ||
@@ -175,34 +231,8 @@ public class PdfBoxForm {
         
         // We do submit controls last as we need all the fields in this form.
         for (Control ctrl : submits) {
-            PDPushButton btn = new PDPushButton(acro);
-            btn.setPushButton(true);
-            PDAnnotationWidget widget = btn.getWidgets().get(0);
-            
-            Rectangle2D rect2D = PdfBoxLinkManager.createTargetArea(ctrl.c, ctrl.box, ctrl.pageHeight, ctrl.transform, root, od);
-            PDRectangle rect = new PDRectangle((float) rect2D.getMinX(), (float) rect2D.getMinY(), (float) rect2D.getWidth(), (float) rect2D.getHeight());
-
-            widget.setRectangle(rect);
-            widget.setPage(ctrl.page);
-            
-            PDFileSpecification fs = PDFileSpecification.createFS(new COSString(element.getAttribute("action")));
-
-            COSArrayList<String> fieldsToInclude = new COSArrayList<String>();
-            fieldsToInclude.addAll(controlNames);
-            
-            PDActionSubmitForm submit = new PDActionSubmitForm();
-            submit.setFields(fieldsToInclude.toList());
-            submit.setFile(fs);
-
-            if (!element.getAttribute("method").equalsIgnoreCase("post")) {
-                // Default method is get.
-                submit.setFlags(1 << 4); // The get flag is bit 4.
-            }
-
-            widget.setAction(submit);
-
-            ctrl.page.getAnnotations().add(widget);
-            acro.getFields().add(btn);
+            i++;
+            processSubmitControl(acro, i, ctrl, root, od);
         }
         
         return i;
