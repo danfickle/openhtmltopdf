2a2a2e8f22f23dfd976f100e654294dc72a01479
Emmeran Seehuber <rototor@rototor.de>
2018-02-16 08:42:24 +0100

    Use options with error reporting, to be able to understand why something is not rendered as intended.

--- openhtmltopdf-latex-support/src/main/java/com/openhtmltopdf/latexsupport/DOMConverter.java
+++ openhtmltopdf-latex-support/src/main/java/com/openhtmltopdf/latexsupport/DOMConverter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.w3c.dom.Element;
 
+import uk.ac.ed.ph.snuggletex.DOMOutputOptions;
 import uk.ac.ed.ph.snuggletex.SnuggleEngine;
 import uk.ac.ed.ph.snuggletex.SnuggleInput;
 import uk.ac.ed.ph.snuggletex.SnuggleSession;
@@ -29,8 +30,10 @@ class DOMConverter {
 		while (latexElement.getChildNodes().getLength() != 0)
 			latexElement.removeChild(latexElement.getFirstChild());
 
+		DOMOutputOptions options = new DOMOutputOptions();
+		options.setErrorOutputOptions(DOMOutputOptions.ErrorOutputOptions.XHTML);
 		try {
-			session.buildDOMSubtree(latexElement);
+			session.buildDOMSubtree(latexElement, options);
 		} catch (Exception e) {
 			throw new IOException("Error while building DOM for: " + rawInputLaTeX + ": " + e.getMessage(), e);
 		}
