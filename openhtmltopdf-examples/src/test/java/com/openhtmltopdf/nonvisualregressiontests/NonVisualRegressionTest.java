543ef9e499c6ef7430891817cddb594c7115c50d
danfickle <newtab@gmail.com>
2018-11-11 14:47:33 +1100

    #180 - Test for link after overflow page.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -295,13 +295,38 @@ public class NonVisualRegressionTest {
         
         remove("link-transform-target");
     }
+    
+    /**
+     * Tests that a simple block link successfully links to an element that is after an inserted overflow page.
+     */
+    @Test
+    public void testLinkAfterOverflowTarget() throws IOException {
+        PDDocument doc = run("link-after-overflow-target");
+        assertEquals(1, doc.getPage(0).getAnnotations().size());
+        assertThat(doc.getPage(0).getAnnotations().get(0), instanceOf(PDAnnotationLink.class));
+        
+        // LINK: Top of first page, 100px by 10px (page margin is 10px).
+        PDAnnotationLink link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(0);
+        assertThat(link.getRectangle(), rectEquals(new PDRectangle(10f, 10f, 80f, 10f), 100d));
+        
+        assertThat(link.getAction(), instanceOf(PDActionGoTo.class));
+        PDActionGoTo action = (PDActionGoTo) link.getAction();
+        
+        assertThat(action.getDestination(), instanceOf(PDPageXYZDestination.class));
+        PDPageXYZDestination dest = (PDPageXYZDestination) action.getDestination();
+        
+        // TARGET: Top of third page.
+        assertEquals(doc.getPage(2), dest.getPage());
+        assertEquals(cssPixelYToPdfPoints(10, 100), dest.getTop(), 1.0d);
+        
+        remove("link-after-overflow-target");
+    }
 
 
     // TODO:
     // + Link to external URL
     // + Link over multiple lines (ie. not simple box).
     // + Link simple inline
-    // + Link with target after generated overflow pages.
     // + Link with target on generated overflow page.
     // + Link with active area on generated overflow page.
     // + Link with active area after generated overflow pages.
