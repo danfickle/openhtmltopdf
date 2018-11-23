f14b58e0357e6b2b06262fbe0503fe14b53996e9
danfickle <newtab@gmail.com>
2018-11-23 20:47:50 +1100

    #180 - Tests link area in page margin.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -441,6 +441,25 @@ public class NonVisualRegressionTest {
         remove("link-external-url");
     }
     
+    /**
+     * Tests a link area in the page margin.
+     */
+    @Test
+    public void testLinkAreaPageMargin() throws IOException {
+        PDDocument doc = run("link-area-page-margin");
+        
+        assertEquals(1, doc.getPage(0).getAnnotations().size());
+        assertEquals(1, doc.getPage(1).getAnnotations().size());
+        
+        PDAnnotationLink link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(0);
+        assertThat(link.getRectangle(), rectEquals(new PDRectangle(170f, 80f, 30f, 10f), 100d));
+
+        // Should be repeated on page 2.
+        link = (PDAnnotationLink) doc.getPage(1).getAnnotations().get(0);
+        assertThat(link.getRectangle(), rectEquals(new PDRectangle(170f, 80f, 30f, 10f), 100d));
+        
+        remove("link-area-page-margin");
+    }
     
     // TODO:
     // + Link over multiple lines (ie. not simple box).
