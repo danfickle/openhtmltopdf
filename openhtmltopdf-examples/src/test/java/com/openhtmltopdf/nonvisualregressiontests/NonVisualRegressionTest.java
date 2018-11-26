9f68bdc4400ec3d1bd9ba9467edb48ba75cac75a
danfickle <newtab@gmail.com>
2018-11-26 16:53:37 +1100

    #180 - Test for link area nested in transformed element in content area.
    This one works, despite not working in the page margin.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -484,6 +484,21 @@ public class NonVisualRegressionTest {
         remove("link-area-page-margin-transform", doc);
     }
     
+    /**
+     * Tests a link element inside a transformed element in the page content.
+     */
+    @Test
+    public void testLinkAreaTransformNested() throws IOException {
+        PDDocument doc = run("link-area-transform-nested");
+        assertEquals(1, doc.getPage(0).getAnnotations().size());
+        assertThat(doc.getPage(0).getAnnotations().get(0), instanceOf(PDAnnotationLink.class));
+        
+        PDAnnotationLink link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(0);
+        assertThat(link.getRectangle(), rectEquals(new PDRectangle(60f, 62f, 50f, 100f), 200d));
+
+        remove("link-area-transform-nested", doc);
+    }
+    
     // TODO:
     // + Link over multiple lines (ie. not simple box).
     // + Link over multiple pages.
