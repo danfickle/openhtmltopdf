dd4549958ffc6b999475a485ca7c8ea7f9b0657b
danfickle <newtab@gmail.com>
2018-11-30 18:07:26 +1100

    #180 Test for link area over mutliple lines.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -529,8 +529,34 @@ public class NonVisualRegressionTest {
         remove("link-area-multiple-page", doc);
     }
     
+    /**
+     * Tests that an inline link over multiple lines generates at least one link annotation for each line.
+     */
+    @Test
+    public void testLinkAreaMultipleLine() throws IOException {
+        PDDocument doc = run("link-area-multiple-line");
+        
+        // One link annotation for each line.
+        assertEquals(2, doc.getPage(0).getAnnotations().size());
+        
+        // First line. Confirmed by looking at the resulting PDF.
+        PDAnnotationLink link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(0);
+        assertEquals(6.0, link.getRectangle().getLowerLeftX(), 1.0d);
+        assertEquals(130.012, link.getRectangle().getLowerLeftY(), 1.0d);
+        assertEquals(138.6, link.getRectangle().getUpperRightX(), 1.0d);
+        assertEquals(144.0, link.getRectangle().getUpperRightY(), 1.0d);
+        
+        // Second line runs out of text before right side of page.
+        link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(1);
+        assertEquals(6.0, link.getRectangle().getLowerLeftX(), 1.0d);
+        assertEquals(116.02, link.getRectangle().getLowerLeftY(), 1.0d);
+        assertEquals(101.13, link.getRectangle().getUpperRightX(), 1.0d);
+        assertEquals(130.01, link.getRectangle().getUpperRightY(), 1.0d);
+        
+        remove("link-area-multiple-line", doc);
+    }
+    
     // TODO:
-    // + Link over multiple lines (ie. not simple box).
     // + Link simple inline target/area.
     // + Form controls plus on/after overflow page.
     // + Custom meta info.
