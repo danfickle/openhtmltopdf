8a4ab06ef3030b37a1c2ff0b23ae43c61d9d0335
danfickle <newtab@gmail.com>
2018-11-30 18:15:41 +1100

    #180 Test for inline link area comprising multiple inline boxes.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -556,6 +556,34 @@ public class NonVisualRegressionTest {
         remove("link-area-multiple-line", doc);
     }
     
+    /**
+     * Tests that an inline link with multiple inline boxes generates one link annotation for each line.
+     * ie. Multiple inline boxes are concatenated into one rect for the purposes of creating a link area.
+     */
+    @Test
+    public void testLinkAreaMultipleBoxes() throws IOException {
+        PDDocument doc = run("link-area-multiple-boxes");
+        
+        // One link annotation for each line.
+        assertEquals(2, doc.getPage(0).getAnnotations().size());
+        
+        // First line. Confirmed by looking at the resulting PDF.
+        PDAnnotationLink link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(0);
+        assertEquals(6.0, link.getRectangle().getLowerLeftX(), 1.0d);
+        assertEquals(130.012, link.getRectangle().getLowerLeftY(), 1.0d);
+        assertEquals(138.6, link.getRectangle().getUpperRightX(), 1.0d);
+        assertEquals(144.0, link.getRectangle().getUpperRightY(), 1.0d);
+        
+        // Second line runs out of text before right side of page.
+        link = (PDAnnotationLink) doc.getPage(0).getAnnotations().get(1);
+        assertEquals(6.0, link.getRectangle().getLowerLeftX(), 1.0d);
+        assertEquals(113.28, link.getRectangle().getLowerLeftY(), 1.0d);
+        assertEquals(112.57, link.getRectangle().getUpperRightX(), 1.0d);
+        assertEquals(127.27, link.getRectangle().getUpperRightY(), 1.0d);
+        
+        remove("link-area-multiple-boxes", doc);
+    }
+    
     // TODO:
     // + Link simple inline target/area.
     // + Form controls plus on/after overflow page.
