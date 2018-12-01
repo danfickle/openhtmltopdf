3bc5eb61b38ef5bdad60be527faecc50ccd4ec79
danfickle <newtab@gmail.com>
2018-12-01 14:19:48 +1100

    #180 - Tests a form control appearing after an overflow page.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -658,7 +658,33 @@ public class NonVisualRegressionTest {
         remove("form-control-overflow-page", doc);
     }
     
+    /**
+     * Tests the positioning, size, name and value of a form control appearing after an overflow page.
+     */
+    @Test
+    public void testFormControlAfterOverflowPage() throws IOException {
+        PDDocument doc = run("form-control-after-overflow-page");
+        
+        assertEquals(0, doc.getPage(0).getAnnotations().size());
+        assertEquals(0, doc.getPage(1).getAnnotations().size());
+        assertEquals(1, doc.getPage(2).getAnnotations().size());
+        assertThat(doc.getPage(2).getAnnotations().get(0), instanceOf(PDAnnotationWidget.class));
+        
+        PDAnnotationWidget widget = (PDAnnotationWidget) doc.getPage(2).getAnnotations().get(0);
+        assertThat(widget.getRectangle(), rectEquals(new PDRectangle(13f, 13f, 60f, 30f), 100));
+        
+        PDAcroForm form = doc.getDocumentCatalog().getAcroForm();
+        assertEquals(1, form.getFields().size());
+        assertThat(form.getFields().get(0), instanceOf(PDTextField.class));
+        
+        PDTextField field = (PDTextField) form.getFields().get(0);
+        assertEquals("text-input", field.getFullyQualifiedName());
+        assertEquals("Hello World!", field.getValue());
+        
+        remove("form-control-after-overflow-page", doc);
+    }
+    
     // TODO:
-    // + Form controls plus on/after overflow page.
+    // + More form controls.
     // + Custom meta info.
 }
