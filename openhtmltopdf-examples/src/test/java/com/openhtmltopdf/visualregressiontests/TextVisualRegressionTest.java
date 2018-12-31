dd72a1bef3d417d28a8c094673f8c07da983cab5
danfickle <newtab@gmail.com>
2018-12-31 17:53:00 +1100

    #180 - Test (FAILING, SERIOUS) - The footer element is not rendering. [ci skip]
    Tests that a nested float in a fixed element renders correctly.
    
    Appears to be an ordering issue of the layers as if header and footer swap element order everything works.

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/visualregressiontests/TextVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/visualregressiontests/TextVisualRegressionTest.java
@@ -433,6 +433,17 @@ public class TextVisualRegressionTest {
     }
     
     /**
+     * Tests that a nested float in a fixed element renders correctly.
+     */
+    @Test
+    @Ignore // SERIOUS FAILURE - The footer element is not rendering.
+            // Appears to be an ordering issue of the layers as if header and footer swap element order
+            // everything works!
+    public void testFixedNestedFloat() throws IOException {
+        assertTrue(run("fixed-nested-float"));
+    }
+    
+    /**
      * Tests that a non-paginated table does not output table header, footer or caption on every page.
      */
     @Test
