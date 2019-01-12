c986c70271cfcec44fb05fc994c8c4e112eb1fff
danfickle <newtab@gmail.com>
2019-01-12 23:22:12 +1100

    #79 - Testcase runner for PDF/UA tests and first simple testcase.

---
+++ openhtmltopdf-examples/src/main/java/com/openhtmltopdf/testcases/pdfua/PdfUaTestcaseRunner.java
@@ -0,0 +1,44 @@
+package com.openhtmltopdf.testcases.pdfua;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.pdfbox.io.IOUtils;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import com.openhtmltopdf.visualregressiontests.TextVisualRegressionTest;
+
+public class PdfUaTestcaseRunner {
+    private static void run(String testCase) throws Exception {
+        byte[] htmlBytes = IOUtils
+                .toByteArray(PdfUaTestcaseRunner.class.getResourceAsStream("/testcases/pdfua/" + testCase + ".html"));
+        String html = new String(htmlBytes, StandardCharsets.UTF_8);
+        
+        new File("./target/pdfua-test-cases/").mkdirs();
+        
+        if (!(new File("target/test/visual-tests/Karla-Bold.ttf")).exists()) {
+            try (InputStream in = TextVisualRegressionTest.class.getResourceAsStream("/visualtest/html/fonts/Karla-Bold.ttf");
+                    OutputStream out = new FileOutputStream("target/test/visual-tests/Karla-Bold.ttf")) {
+                    IOUtils.copy(in, out);
+            }
+        }
+        
+        try (FileOutputStream os = new FileOutputStream("./target/pdfua-test-cases/" + testCase + ".pdf")) {
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.testMode(true);
+            builder.usePdfUaAccessbility(true);
+            builder.useFont(new File("target/test/visual-tests/Karla-Bold.ttf"), "TestFont");
+            builder.withHtmlContent(html, PdfUaTestcaseRunner.class.getResource("/testcases/pdfua/").toString());
+            builder.toStream(os);
+            builder.run();
+        }
+        
+    }
+    
+    public static void main(String... args) throws Exception {
+        run("simple");
+    }
+}
