91f6498df96532dfd3990ef9ed8c47965792c673
danfickle <newtab@gmail.com>
2019-07-24 18:23:35 +1000

    Get rid of tabs in the flowing column implementation.
    So it can be worked with easier.

--- openhtmltopdf-core/src/main/java/com/openhtmltopdf/render/FlowingColumnBox.java
+++ openhtmltopdf-core/src/main/java/com/openhtmltopdf/render/FlowingColumnBox.java
@@ -1,34 +1,34 @@
 package com.openhtmltopdf.render;
 
 public class FlowingColumnBox extends BlockBox {
-	private final Box parent;
-	private int _colWidth;
-	
-	public FlowingColumnBox(Box parent) {
-		this.parent = parent;
-	}
-	
-	@Override
-	public boolean isFlowingColumnBox() {
-		return true;
-	}
-	
-	@Override
-	public int getWidth() {
-		return this._colWidth;
-	}
-	
-	@Override
-	public int getContentWidth() {
-		return this._colWidth;
-	}
+    private final Box parent;
+    private int _colWidth;
 
-	@Override
-	public Box getParent() {
-		return parent;
-	}
+    public FlowingColumnBox(Box parent) {
+        this.parent = parent;
+    }
 
-	public void setColumnWidth(int columnWidth) {
-		this._colWidth = columnWidth;
-	}
+    @Override
+    public boolean isFlowingColumnBox() {
+        return true;
+    }
+
+    @Override
+    public int getWidth() {
+        return this._colWidth;
+    }
+
+    @Override
+    public int getContentWidth() {
+        return this._colWidth;
+    }
+
+    @Override
+    public Box getParent() {
+        return parent;
+    }
+
+    public void setColumnWidth(int columnWidth) {
+        this._colWidth = columnWidth;
+    }
 }
