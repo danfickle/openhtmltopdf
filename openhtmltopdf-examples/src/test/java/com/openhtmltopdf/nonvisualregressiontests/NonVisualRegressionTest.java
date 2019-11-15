fe322914c9f6a1ecec90dba20ffcddd365fcd6db
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-11-15 15:18:46 +0100

    handle bookmarks in body #408

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -162,6 +162,26 @@ public class NonVisualRegressionTest {
     }
 
     /**
+     * Tests that a simple body bookmark linking to top of the second page works.
+     */
+    @Test
+    public void testBookmarkBodySimple() throws IOException {
+        PDDocument doc = run("bookmark-body-simple");
+        PDDocumentOutline outline = doc.getDocumentCatalog().getDocumentOutline();
+
+        PDOutlineItem bm = outline.getFirstChild();
+        assertThat(bm.getTitle(), equalTo("Test bookmark"));
+        assertThat(bm.getDestination(), instanceOf(PDPageXYZDestination.class));
+        PDPageXYZDestination dest = (PDPageXYZDestination) bm.getDestination();
+
+        // At top of second page.
+        assertEquals(dest.getPage(), doc.getPage(1));
+        assertEquals(doc.getPage(1).getMediaBox().getUpperRightY(), dest.getTop(), 1.0d);
+
+        remove("bookmark-body-simple", doc);
+    }
+
+    /**
      * Tests that a head bookmark linking to transformed element (by way of transform) on third page works. 
      */
     @Test
