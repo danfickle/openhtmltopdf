72b50a481f7fafe8d167dd8163744a618786e8ca
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-11-17 23:57:25 +0100

    handle more gracefully an input without name attribute + select without option child #151

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/nonvisualregressiontests/NonVisualRegressionTest.java
@@ -702,6 +702,21 @@ public class NonVisualRegressionTest {
         
         remove("form-control-after-overflow-page", doc);
     }
+
+    /**
+     * Check that an input without name attribute does not launch a NPE.
+     * Will now log a warning message.
+     * See issue: https://github.com/danfickle/openhtmltopdf/issues/151
+     *
+     * Additionally, check that a select element without options will not launch a NPE too.
+     */
+    @Test
+    public void testInputWithoutNameAttribute() throws IOException {
+        PDDocument doc = run("input-without-name-attribute");
+        PDAcroForm form = doc.getDocumentCatalog().getAcroForm();
+        assertEquals(0, form.getFields().size());
+        remove("input-without-name-attribute", doc);
+    }
     
     // TODO:
     // + More form controls.
