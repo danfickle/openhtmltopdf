7b223c2d5d78504f3d76f8c6a8323b5bc15fc477
danfickle <newtab@gmail.com>
2020-07-11 21:44:02 +1000

    #489 Make slf4j-api provided scope and some tests of using slf4j module.
    Making slf4j-api provided scope should reduce dependency hell for end users as almost all will already have slf4j-api on their classpath.
    
    Also added a couple of manual tests to make sure module is working as expected.

---
+++ openhtmltopdf-slf4j/src/test/java/com/openhtmltopdf/slf4j/Slf4JLoggerTest.java
@@ -0,0 +1,79 @@
+package com.openhtmltopdf.slf4j;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import com.openhtmltopdf.util.XRLog;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class Slf4JLoggerTest {
+
+    @BeforeClass
+    public static void levelFormat() {
+        // We use this property to differentiate log messages
+        // going through slf4j to those going through the default
+        // java.util.logging logger implementation.
+        System.setProperty("org.slf4j.simpleLogger.levelInBrackets", "true");
+    }
+
+    /**
+     * This is a simple automatic test to ensure log messages are
+     * delivered by slf4j in the console.
+     */
+    @Test
+    public void testLogger() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos, true, "UTF-8");
+        PrintStream old = System.err;
+        System.setErr(ps);
+
+        XRLog.setLoggerImpl(new Slf4jLogger());
+        runWithLogOutput();
+
+        ps.flush();
+        String log = baos.toString("UTF-8");
+
+        old.println(log);
+        System.setErr(old);
+
+        Assert.assertThat(log, containsString("] [INFO]"));
+        Assert.assertThat(log, containsString("] [WARN]"));
+    }
+
+    /**
+     * This is a manual test to show that log level can be changed
+     * by the concrete log implementation.
+     * 
+     * It should not output info level messages.
+     * 
+     * NOTE: This test should be run in isolation as the system property
+     * only takes affect if set before the logger is first created.
+     */
+    @Test
+    public void testLoggerWarnLevel() throws IOException {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
+        XRLog.setLoggerImpl(new Slf4jLogger());
+
+        runWithLogOutput();
+    }
+
+    private void runWithLogOutput() throws IOException {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+
+            builder.useFastMode();
+            builder.toStream(os);
+            builder.withHtmlContent("<>INVALID-XML", null);
+            builder.run();
+        } catch (Exception e) {
+            // Ignore.
+        }
+    }
+}
