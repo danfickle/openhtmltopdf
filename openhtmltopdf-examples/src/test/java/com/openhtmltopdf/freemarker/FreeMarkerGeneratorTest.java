b2ef8a3e006c59ab43b8123b7f4d051b6f425164
danfickle <newtab@gmail.com>
2021-05-13 16:36:02 +1000

    Tabs to spaces in freemarker test class

--- openhtmltopdf-examples/src/test/java/com/openhtmltopdf/freemarker/FreeMarkerGeneratorTest.java
+++ openhtmltopdf-examples/src/test/java/com/openhtmltopdf/freemarker/FreeMarkerGeneratorTest.java
@@ -22,40 +22,39 @@ public class FreeMarkerGeneratorTest {
         TestSupport.quietLogs();
     }
 
-	@SuppressWarnings("ResultOfMethodCallIgnored")
-	@Test
-	public void testFreeMarkerGenerator() throws IOException, TemplateException {
-		File targetDir = new File("target/test/freemarker");
-		targetDir.mkdirs();
-		FreeMarkerGenerator freeMarkerGenerator = new FreeMarkerGenerator();
-		FreemarkerRootObject object = new FreemarkerRootObject();
-		String html = freeMarkerGenerator.generateHTML("featuredocumentation.ftl", Locale.GERMAN, object);
-		byte[] pdf = freeMarkerGenerator.generatePDF(html);
-		FileOutputStream fileOutputStream = new FileOutputStream(new File(targetDir, "featuredocumentation.html"));
-		fileOutputStream.write(html.getBytes(StandardCharsets.UTF_8));
-		fileOutputStream.close();
-		fileOutputStream = new FileOutputStream(new File(targetDir, "featuredocumentation.pdf"));
-		fileOutputStream.write(pdf);
-		fileOutputStream.close();
-	}
+    @Test
+    public void testFreeMarkerGenerator() throws IOException, TemplateException {
+        File targetDir = new File("target/test/freemarker");
+        targetDir.mkdirs();
+        FreeMarkerGenerator freeMarkerGenerator = new FreeMarkerGenerator();
+        FreemarkerRootObject object = new FreemarkerRootObject();
+        String html = freeMarkerGenerator.generateHTML("featuredocumentation.ftl", Locale.GERMAN, object);
+        byte[] pdf = freeMarkerGenerator.generatePDF(html);
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(targetDir, "featuredocumentation.html"));
+        fileOutputStream.write(html.getBytes(StandardCharsets.UTF_8));
+        fileOutputStream.close();
+        fileOutputStream = new FileOutputStream(new File(targetDir, "featuredocumentation.pdf"));
+        fileOutputStream.write(pdf);
+        fileOutputStream.close();
+    }
 
-	@Test
-	public void testFreeMarkerWithManyPages() throws IOException, TemplateException {
-		/*
-		 * We really should disable logging here, as it takes ages anyway to generate
-		 * the report...
-		 */
-		XRLog.setLoggingEnabled(false);
-		File targetDir = new File("target/test/freemarker");
-		targetDir.mkdirs();
-		FreeMarkerGenerator freeMarkerGenerator = new FreeMarkerGenerator();
-		FreemarkerRootObject object = new FreemarkerRootObject();
-		File htmlFile = new File(targetDir, "many_pages.html");
-		freeMarkerGenerator.generateHTMLToFile("many_pages.ftl", Locale.GERMAN, object, htmlFile);
-		byte[] pdf = freeMarkerGenerator.generatePDF(htmlFile);
-		FileOutputStream fileOutputStream = new FileOutputStream(new File(targetDir, "many_pages.pdf"));
-		fileOutputStream.write(pdf);
-		fileOutputStream.close();
-		XRLog.setLoggingEnabled(true);
-	}
+    @Test
+    public void testFreeMarkerWithManyPages() throws IOException, TemplateException {
+        /*
+         * We really should disable logging here, as it takes ages anyway to generate
+         * the report...
+         */
+        XRLog.setLoggingEnabled(false);
+        File targetDir = new File("target/test/freemarker");
+        targetDir.mkdirs();
+        FreeMarkerGenerator freeMarkerGenerator = new FreeMarkerGenerator();
+        FreemarkerRootObject object = new FreemarkerRootObject();
+        File htmlFile = new File(targetDir, "many_pages.html");
+        freeMarkerGenerator.generateHTMLToFile("many_pages.ftl", Locale.GERMAN, object, htmlFile);
+        byte[] pdf = freeMarkerGenerator.generatePDF(htmlFile);
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(targetDir, "many_pages.pdf"));
+        fileOutputStream.write(pdf);
+        fileOutputStream.close();
+        XRLog.setLoggingEnabled(true);
+    }
 }
