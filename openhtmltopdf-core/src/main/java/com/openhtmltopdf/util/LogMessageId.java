77a2592a9c45ccf6557304f6b4912eb4364b767b
smahnken <105445999+smahnken@users.noreply.github.com>
2022-05-25 23:58:31 -0600

    Adding autorotation for JPEG
    This change adds automatic image rotation for JPEG images with orientation EXIF data.
    
    Almost all cell phone cameras store the image data in an un-rotated state, and write EXIF data to indicate "which side should be up".  All modern browsers understand these EXIF tags and automatically orient the image correctly.  Thus, when the pictures are viewed in a web page they appear correct.  When the page is output to PDF, however, the image is rotated.
    
    This change modifies the pdfboximage class to read the EXIF data (if it exists) and rotate the image if necessary.  This results in a "rotate on load" behavior (i.e. before the resource cache), so the rotation only needs to be done once, even if the image appears multiple times in the document.

--- openhtmltopdf-core/src/main/java/com/openhtmltopdf/util/LogMessageId.java
+++ openhtmltopdf-core/src/main/java/com/openhtmltopdf/util/LogMessageId.java
@@ -30,7 +30,7 @@ public interface LogMessageId {
         LAYOUT_ERROR_PARSING_SHAPE_COORDS(XRLog.LAYOUT, "Error while parsing shape coords"),
         LAYOUT_NO_CONTENT_LIMIT_FOUND(XRLog.LAYOUT, "No content limit found"),
         LAYOUT_BOX_HAS_NO_PAGE(XRLog.LAYOUT, "Box has no page"),
-        LAYOUT_NO_INLINE_LAYERS(XRLog.LAYOUT, "Boxes with display: inline can not be positioned or transformed, try using inline-block"),
+		LAYOUT_NO_INLINE_LAYERS(XRLog.LAYOUT, "Boxes with display: inline can not be positioned or transformed, try using inline-block"),
 
         CASCADE_IS_ABSOLUTE_CSS_UNKNOWN_GIVEN(XRLog.CASCADE, "Asked whether type was absolute, given CSS_UNKNOWN as the type. " +
                 "Might be one of those funny values like background-position."),
@@ -47,6 +47,7 @@ public interface LogMessageId {
         GENERAL_PDF_USING_GET_REQUEST_FOR_FORM(XRLog.GENERAL, "Using GET request method for form. You probably meant to add a method=\"post\" attribute to your form"),
         GENERAL_PDF_ACROBAT_READER_DOES_NOT_SUPPORT_FORMS_WITH_FILE_INPUT(XRLog.GENERAL, "Acrobat Reader does not support forms with file input controls"),
 
+        EXCEPTION_IMAGE_METADATA_COULD_NOT_BE_FOUND(XRLog.EXCEPTION, "Image metadata could not be found."),
         EXCEPTION_SVG_COULD_NOT_DRAW(XRLog.EXCEPTION, "Couldn't draw SVG."),
         EXCEPTION_SVG_COULD_NOT_READ_FONT(XRLog.EXCEPTION, "Couldn't read font"),
         EXCEPTION_MATHML_COULD_NOT_REGISTER_FONT(XRLog.EXCEPTION, "Could not register font correctly"),
